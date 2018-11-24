844dc718cee7d4215aef63f078a89f99090d207a
GiselaMD <gisela_difini@hotmail.com>
2018-11-24 16:31:17 -0200

    server and client

---
+++ client/src/components/Category.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { getAllPostsCategoryAction } from '../actions'
+import PostCard from './PostCard';
+import './Category.css';
+import './PostsList';
+import {Button, Row, Col} from 'react-bootstrap'
+import {FaAngleDoubleRight} from 'react-icons/fa'
+import OrderBy from './OrderBy';
+import CategoryList from './CategoryList';
+class Category extends Component {
+    render() {    
+      this.props.getPostsCategory(this.props.match.params.category)
+
+      return(
+        <Row>
+          <Col md={2}> </Col>
+          <Col md={8}> 
+          <h4>Categoria: {this.props.match.params.category}</h4>
+            <OrderBy/>
+            {
+           (this.props.posts && this.props.posts.length > 0) 
+           ? (  this.props.posts.map(post => 
+                    <div className="container" key={post.id}>
+                        <PostCard postId={post.id} post={post} score={post.voteScore}/>
+                        <p className='text_btn_openpost'>
+                            <Link to={`/${post.category}/${post.id}`} >
+                                <Button className='btn_openpost' block>
+                                    <strong>Open Post</strong> <FaAngleDoubleRight></FaAngleDoubleRight>
+                                </Button>
+                            </Link>
+                        </p>
+                    </div>
+                )
+            ): 
+          (
+            <div className="container">
+               <h2 className="text_center">No Posts yet :(</h2> 
+            </div>
+          )
+          }
+          </Col>
+          <Col md={2}><CategoryList/></Col>
+        </Row>
+        
+      )
+    }
+  }
+  
+  const mapStateToProps = ({posts}) => {
+    return {
+      posts: posts
+    }
+  }
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      getPostsCategory: (category) => dispatch(getAllPostsCategoryAction(category))
+    }
+  }
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(Category)
---
+++ client/src/components/EditPost.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import {Redirect} from 'react-router-dom'
+import './Form.css';
+import { Row, Col, Button} from 'react-bootstrap'
+import { updatePost, fetchPost } from '../actions';
+
+class EditPost extends Component {
+  state = {
+    notValid: false,
+    success: false
+  }
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
+
+  editCurrentPost = e => {
+    e.preventDefault();
+
+    const title = e.target.title.value
+    const body = e.target.body.value
+    const postId = this.props.match.params.id
+
+    if (title && body) {
+      this.props.updatePost(postId, title, body)
+      
+      this.setState({success: true})
+    
+    } else {
+      this.setState({
+        notValid: true,
+        success: false
+      })
+    }
+  }
+
+  handleCategoryChange = (e) => {
+    this.setState({
+      category: e.target.value
+    })
+    
+  }
+    render() {
+      const { post } = this.props
+
+        return post ? (
+        <div>
+          <Row>
+            <Col md={3}></Col>
+            <Col md={6}>
+            {this.state.success ? (
+                  <Redirect to="/" />
+            ) : 
+            (
+              <div className="form_container">
+                <div>
+                  {this.state.notValid && (
+                    <h3>Please enter all values...</h3> 
+                  )}
+                </div>
+              <h2>Edit post</h2><br/>
+              <form onSubmit={this.editCurrentPost}>
+                <label>
+                  Title:
+                  <input 
+                  placeholder={post.title}
+                  name='title' 
+                  defaultValue={post.title}/>
+                </label> 
+                <br/>
+                <label>
+                  <textarea  
+                  placeholder={post.body}
+                  name='body'
+                  defaultValue={post.body}
+                  cols="30" 
+                  rows="8" />
+                </label>
+              
+                <Button block type="submit">Submit</Button>
+              </form>
+              </div>
+            )}
+            
+            </Col>
+            <Col md={3}></Col>
+          </Row>
+       
+        </div>
+      ) : (
+        <div className="container">
+            <h2 className="text_center">Error 404</h2> 
+        </div>
+      )
+    }
+    
+  }
+
+  const mapStateToProps = ({posts, categories}, ownProps) => ({
+    post: posts.find(p => p.id === ownProps.match.params.id),
+    categories: categories
+  });
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      getPost: (id) => dispatch(fetchPost(id)),
+      updatePost: (postId, title, body) => dispatch(updatePost(postId, title, body))
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditPost)
